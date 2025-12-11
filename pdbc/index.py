946fb39899b93b5073441956294b567f3cffd2e3
HARISHMALISETTY <malisettyharish@gmail.com>
2025-12-11 15:08:28 +0530

    updating and retrieving data

--- pdbc/index.py
+++ pdbc/index.py
@@ -30,26 +30,64 @@ def createTable():
 
     except Exception as e:
         print("error while creating table",e)
+
 def insertData():
     try:
         name=input("enter student name: ")
         age=int(input("enter age : "))
         gender=input("enter gender : ")
         email=input("enter email : ")
-        cursor.execute("use 66r_68r")
-        cursor.execute(f""" 
-                    insert into students values({name},{age},{gender},{email});
-        """)
+        cursor.execute("use 66r_68r")        
+        query="""insert into students (name,age,gender,email) values (%s,%s,%s,%s)"""
+        values=(name,age,gender,email)
+        cursor.execute(query,values)
         print("data inserted successfully into table")
     except Exception as e:
         print("error occured while inserting data",e)
+def insertManyRecords():
+    try:
+        cursor.execute("use 66r_68r")
+        data=[('harish',26,'male','harish@gmail.com'),('nandu',25,'female','nandu@gmail.com'),('sushma',25,'female','sushma@gmail.com')]
+        query="""insert into students (name,age,gender,email) values (%s,%s,%s,%s)"""
+        cursor.executemany(query,data)
+        print("data inserted successfully")
+    except Exception as e:
+        print('error occured while inserting data',e)
+
+def updateStudentEmailById():
+    try:
+        cursor.execute('use 66r_68r')
+        stud_id=int(input("enter student id"))
+        email=input("enter email: ")
+        query="""update students set email=%s where id =%s"""
+        values=(email,stud_id)
+        cursor.execute(query,values)
+        print("record updated successfully")
+    except Exception as e:
+        print("error occured",e)
+
+#updatestudentnamebyid
+#updatestudentagebyid
+#deleterecordbyid
+
 def menu():
     print("choose 1 for creating new table: ")
+    print("choose 2 for inserting data")
+    print("choose 3 for fetching records")
+    print("choose 4 for inserting multiple records")
+    print("choose 5 for updating record")
+
     ip=int(input("enter choice"))
     if ip==1:
         createTable()
     elif ip==2:
         insertData()
+    elif ip==3:
+        getData()
+    elif ip==4:
+        insertManyRecords()
+    elif ip==5:
+        updateStudentEmailById()
     else:
         print("wrong option")
 
